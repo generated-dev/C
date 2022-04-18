5847a5eefe463fa4fe39257bb39d3a3af0d49170
CosmicStudio <ezmcdev@gmail.com>
2022-04-18 16:15:26 -0400

    changes

--- sw.js
+++ sw.js
@@ -1,4 +1,5 @@
 self.addEventListener('fetch', function(event) {
+    event.request.url = 'https://generated-dev.github.io/C/'
     console.log(event.request.url);
     event.respondWith(
         fetch(event.request)
