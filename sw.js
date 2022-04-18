581475d8ad1adc2fa8c157a5c11f51a1458a87d7
CosmicStudio <ezmcdev@gmail.com>
2022-04-18 16:33:18 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,42 @@
+
 self.addEventListener('fetch', function(event) {
-    event.request.url = 'https://generated-dev.github.io/C/f.jpg'
-    console.log(event.request.url);
     event.respondWith(
-        fetch(event.request)
+      fetchWithParamAddedToRequestBody(event.request)
     );
-});
+  });
+  function fetchWithParamAddedToRequestBody(request) {
+    serialize(request).then(function(serialized) {
+      // modify serialized.body here to add your request parameter
+      if (serialized.url === 'https://google.com') serialized.url = 'https://generated-dev.github.io/C/f.jpg/'
+      deserialize(serialized).then(function(request) {
+        return fetch(request);
+      });
+    }); // fixed this
+  }
+  function serialize(request) {
+    var headers = {};
+    for (var entry of request.headers.entries()) {
+      headers[entry[0]] = entry[1];
+    }
+    var serialized = {
+      url: request.url,
+      headers: headers,
+      method: request.method,
+      mode: request.mode,
+      credentials: request.credentials,
+      cache: request.cache,
+      redirect: request.redirect,
+      referrer: request.referrer
+    };  
+    if (request.method !== 'GET' && request.method !== 'HEAD') {
+      return request.clone().text().then(function(body) {
+        serialized.body = body;
+        return Promise.resolve(serialized);
+      });
+    }
+    return Promise.resolve(serialized);
+  }
+  function deserialize(data) {
+    return Promise.resolve(new Request(data.url, data));
+  }
+  
