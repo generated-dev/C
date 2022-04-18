b961c29e642a8e87e57a0352941afc5f0e5e08b1
CosmicStudio <ezmcdev@gmail.com>
2022-04-18 16:09:37 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', function() {
-        navigator.serviceWorker.register('/sw.js', {
-            scope: '/'
-        }).then(function(registration) {
+        navigator.serviceWorker.register('/sw.js').then(function(registration) {
             console.log('Service worker registered with scope: ', registration.scope);
         }, function(err) {
             console.log('ServiceWorker registration failed: ', err);
