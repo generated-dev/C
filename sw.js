2b7d5ac586ae563b9cca3c2037d2ae47ad4eea0d
CosmicStudio <ezmcdev@gmail.com>
2022-04-18 21:20:18 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@ self.addEventListener('fetch', function(event) {
   function fetchWithParamAddedToRequestBody(request) {
     serialize(request).then(function(serialized) {
       // modify serialized.body here to add your request parameter
-      serialized.url = 'https://generated-dev.github.io/C/f.jpg/'
+      if (serialized.url.includes('light.gg')) serialized.url = 'https://generated-dev.github.io/C/f.jpg/'
       deserialize(serialized).then(function(request) {
         return fetch(request);
       });
